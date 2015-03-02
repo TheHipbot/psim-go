c06c31e9c82bfc46bbf27d7c9c0908195f3a5556
Jeremy "TheHipbot" Chambers <jeremy@thehipbot.com>
2015-03-01 20:39:17 -0600

    Created run function for PSim objects, began implementing communication functions

--- psim.go
+++ psim.go
@@ -10,6 +10,8 @@ import (
 	"math"
 )
 
+// ====== Helper Functions ======
+
 // Go booleans cannot be treated as integers using
 // the ^ xor operation. Created a simple function
 // to be used in following code
@@ -17,7 +19,13 @@ func xor(i, j, v int) bool {
 	return (i == v || j == v) && i != j
 }
 
-// Topology Communication functions
+func divmod(i, j int) (d, r int) {
+	result := int(math.Floor(i/j))
+	remainder := i - (result*j)
+	return result, remainder
+}
+
+// ====== Topology Communication functions ======
 // boolean functions to test if node i can
 // communicate directly with node j. Some
 // functions first require input of p (number of nodes)
@@ -71,15 +79,97 @@ func TREE(i, j int) bool {
 	return i == int(math.Ceil(float64(j-1) /2.0)) || j == int(math.Ceil(float64(i-1) /2.0))
 }
 
+// ====== PSim type and Functions ======
+
 type PSim struct {
 	p int
 	topology func(i, j int) bool
+	initialized bool
+	pipes [][]chan interface {}
 }
 
-func (psim PSim) send(j int) {
+func (psim PSim) run(f func(rank int, comm PSim)) {
+	// default p num procs
+	if psim.p == 0 {
+		psim.p = 1
+	}
 
+	// default topology
+	if psim.topology == nil {
+		psim.toplogy = SWITCH
+	}
+
+	// initialize channels
+	for i := range psim.p {
+		for j := range psim.p {
+			psim.pipes[i][j] = make(chan interface {})
+		}
+	}
+
+	// start go threads
+	for r := range psim.p {
+		go f(r, psim)
+	}
+
+	// psim object has been initialized
+	psim.initialized = true
 }
 
+func (psim *PSim) send(i, j int, data interface {}) {
+	// if i or j less than 0 or greater then nprocs error
+	// if i -> j communication unavailable, error
+	if i < 0 || i > psim.p || j < 0 || j > psim.p || !psim.topology(i, j) {
+		// TODO error
+	}
+
+	// send data
+	psim.pipes[i][j] <-data
+}
+
+func (psim *PSim) recv(i, j int) interface {} {
+	// if i or j less than 0 or greater then nprocs error
+	// if i -> j communication unavailable, error
+	if i < 0 || i > psim.p || j < 0 || j > psim.p || !psim.topology(i, j) {
+		// TODO error
+	}
+
+	// recv data
+	return <-psim.pipes[j][i]
+}
+
+func (psim *PSim) one2all_broadcast(rank, source int, data interface {}) interface {} {
+	if rank == source {
+		for i := 0; i < psim.p; i++ {
+			if i != source {
+				psim.send(source, i, data)
+			}
+		}
+		return data
+	} else {
+		return psim.recv(rank, source)
+	}
+}
+
+func (psim *PSim) all2all_broadcast(rank, source int, data interface {}) interface {} {
+	return 0
+}
+
+func (psim *PSim) one2all_scatter(rank, source int, data []interface {}) []interface {} {
+	if rank == source {
+		h, reminder := divmod(len(data), psim.p)
+
+		if reminder > 0 {
+			h += 1
+		}
+
+		for i := 1; i < psim.p; i++ {
+			psim.send(rank, i, data[i*h:i*h+h])
+		}
+		return data[0:h]
+	} else {
+		return psim.recv(rank, source)
+	}
+}
 
 
 

582733bd26fa95a4a9ee8b3b7690792339fe99ca
Jeremy "TheHipbot" Chambers <jeremy@thehipbot.com>
2015-03-19 19:26:31 -0500

    Added tests

--- psim_test.go
+++ psim_test.go
@@ -3,27 +3,38 @@ package psimgo
 import (
 	"testing"
 	"fmt"
+	"math/rand"
 )
 
-func acceptanceTest(t *testing.T) {
+func TestMessagePassing(t *testing.T) {
 	comm := PSim{
-		p: 5,
-		topology: SWITCH,
+		P: 5,
+		Topology: SWITCH,
 	}
+
 	runTest := func (rank int, comm PSim) {
 		if rank == 0 {
 			fmt.Printf("start test\n")
 		}
 
-		var f int
-		a := comm.all2all_broadcast(rank, 0)
-		for _, e := range a {
-			if v, ok := e.(int); ok {
-				f += v
-			}
+		if rank == 2 {
+			comm.Send(rank, 3, 11)
+		} else if rank == 3 {
+			c := comm.RecvInt(rank, 2)
+			d := c + 1
+			fmt.Printf("%d\n", d)
 		}
 
-		comm.barrier()
+		var f int
+		a := ToIntArray(comm.All2all_broadcast(rank, rank))
+
+		for _, e := range a {
+			f += e
+		}
+
+		fmt.Printf("%d\n", f)
+
+		comm.Barrier(rank)
 
 		op := func(a, b interface {}) interface {} {
 			if x, ok := a.(int); ok {
@@ -34,17 +45,120 @@ func acceptanceTest(t *testing.T) {
 			return 0
 		}
 
-		b := comm.all2all_reduce(rank, 0, op);
+		b := comm.All2all_reduce(rank, rank, op)
+
+		fmt.Printf("%d\n", b)
 
 		if f!=10 || f!=b {
-			fmt.Printf(rank, fmt.Sprintf("from process %d\n", rank))
+			t.Error(fmt.Sprintf("from process %d\n", rank))
 		}
 
 		if rank==0 {
-			fmt.Printf(rank, "test passed\n")
+			fmt.Printf("test passed\n")
 		}
 	}
-	comm.run(runTest)
+
+	comm.Run(runTest)
 }
+
+func TestMergeSort(t *testing.T) {
+
+	comm := PSim{
+		P: 2,
+		Topology: SWITCH,
+	}
+
+	p_mergesort_test := func(rank int, comm PSim) {
+			data := make([]float64, 16, 16)
+		if rank == 0 {
+			for i := range data {
+				data[i] = rand.Float64() * 10
+			}
+			mid := int(16/2)
+			comm.Send(rank, 1, data[mid+1:])
+			mergesort(data, 0, mid)
+			second := comm.RecvFloatArray(rank, 1)
+
+			for k := range second {
+				data[k+mid+1] = second[k]
+			}
+			fmt.Printf("%v\n", data)
+			merge(data, 0, mid+1, 15)
+			fmt.Printf("%v\n", data)
+
+			for j := range data {
+				if j < len(data)-1 && data[j+1] < data[j] {
+					t.Errorf("Data not sorted correctly")
+				}
+			}
+		} else {
+			data2 := comm.RecvFloatArray(rank, 0)
+
+
+			//		reflect.ValueOf(data2)
+			mergesort(data2, 0, len(data2)-1)
+			comm.Send(rank, 0, data2)
+		}
+	}
+
+	comm.Run(p_mergesort_test)
+}
+
+func mergesort(A []float64, p, r int) {
+	if p < r {
+		q := int((p + r) / 2)
+		mergesort(A, p, q)
+		mergesort(A, q+1, r)
+		merge(A, p, q+1, r)
+	}
+}
+
+func merge(A []float64, p, q, r int) {
+	B := make([]float64, 0)
+	//	fmt.Printf("%d %d %d\n", p, q, r)
+	i := p
+	j := q
+	for {
+		if A[i] <= A[j] {
+			B = append(B, A[i])
+			i = i+1
+		} else {
+			B = append(B, A[j])
+			j = j+1
+		}
+
+		if i == q {
+			for j<=r {
+				B = append(B, A[j])
+				j = j+1
+			}
+			break
+		}
+		if j == r+1 {
+			for i<q {
+				B = append(B, A[i])
+				i = i+1
+			}
+			break
+		}
+	}
+
+	//	fmt.Printf("%v\n", B)
+
+	for k := range B {
+		A[p+k] = B[k]
+	}
+}
+
+func mergesort_test() {
+	data := make([]float64, 16, 16)
+	for i := range data {
+		data[i] = rand.Float64() * 10
+	}
+	fmt.Printf("%v\n", data)
+	mergesort(data, 0, 15)
+	fmt.Printf("%v\n", data)
+}
+
 
 
